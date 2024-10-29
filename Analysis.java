126b9ddfeb57eb8f49c5199217729db370ce89d9
Mrigank Pawagi <mrigankpawagi@gmail.com>
2024-10-29 21:12:39 +0530

    cleanup; add LatticeElement interface and IntervalElement class

--- Analysis.java
+++ Analysis.java
@@ -1,12 +1,3 @@
-// This program will plot a CFG for a method using soot
-// [ExceptionalUnitGraph feature].
-// Arguements : <ProcessOrTargetDirectory> <MainClass> <TargetClass> <TargetMethod>
-
-// Ref:
-// 1) https://gist.github.com/bdqnghi/9d8d990b29caeb4e5157d7df35e083ce
-// 2) https://github.com/soot-oss/soot/wiki/Tutorials
-
-
 import java.util.*;
 import soot.options.Options;
 
@@ -35,9 +26,52 @@ import soot.toolkits.graph.BriefUnitGraph;
 import soot.jimple.IfStmt;
 
 public class Analysis {
+    public interface LatticeElement {
+        // Join operation
+        LatticeElement join(LatticeElement other);
+
+        // Application of transfer functions (based on statements)
+        LatticeElement tf_assignment(Stmt stmt);
+    }
+
+    public static class IntervalElement implements LatticeElement {
+        private final float lowerBound;
+        private final float upperBound;
+    
+        public IntervalElement(float lowerBound, float upperBound) {
+            this.lowerBound = lowerBound;
+            this.upperBound = upperBound;
+        }
+    
+        public float getLowerBound() {
+            return lowerBound;
+        }
+    
+        public float getUpperBound() {
+            return upperBound;
+        }
+
+        public LatticeElement join(LatticeElement other) {
+            if (!(other instanceof IntervalElement)) {
+                throw new IllegalArgumentException("Incompatible types for join");
+            }
+            IntervalElement otherInterval = (IntervalElement) other;
+            float newLower = Math.min(this.lowerBound, otherInterval.lowerBound);
+            float newUpper = Math.max(this.upperBound, otherInterval.upperBound);
+            return new IntervalElement(newLower, newUpper);
+        }
+
+        public LatticeElement tf_assignment(Stmt stmt) {
+            return new IntervalElement(Float.NEGATIVE_INFINITY, Float.POSITIVE_INFINITY);
+        }
+
+        public String toString() {
+            return "[" + lowerBound + ", " + upperBound + "]";
+        }
+    }    
+
     private DotGraph dot = new DotGraph("callgraph");
-    private static HashMap<String, Boolean> visited = new
-        HashMap<String, Boolean>();
+    private static HashMap<String, Boolean> visited = new HashMap<String, Boolean>();
 
     public Analysis() {
     }
@@ -68,20 +102,16 @@ public class Analysis {
         }
     }
 
-
-
-    public static void doAnalysis(SootMethod targetMethod){
+    public static void doAnalysis(SootMethod targetMethod) {
         Body body = targetMethod.retrieveActiveBody();
 
         // Get integer variables (considering byte, short, int, long)
         List<Local> integerLocals = new ArrayList<>();
         for (Local local : body.getLocals()) {
-            if (
-                    local.getType() instanceof IntType || 
-                    local.getType() instanceof LongType || 
-                    local.getType() instanceof ByteType || 
-                    local.getType() instanceof ShortType
-                ) {
+            if (local.getType() instanceof IntType ||
+                    local.getType() instanceof LongType ||
+                    local.getType() instanceof ByteType ||
+                    local.getType() instanceof ShortType) {
                 integerLocals.add(local);
             }
         }
@@ -112,7 +142,7 @@ public class Analysis {
         // create a CFG based on program-points
         Map<Integer, Set<Integer>> flowPoints = new HashMap<>();
         int entryPoint = 0;
-        
+
         // map from pairs of program-points to enclosing unit
         Map<Pair<Integer, Integer>, Unit> enclosingUnit = new HashMap<>();
 
@@ -167,13 +197,12 @@ public class Analysis {
 
     public static void main(String[] args) {
 
-        String targetDirectory=args[0];
-        String mClass=args[1];
-        String tClass=args[2];
-        String tMethod=args[3];
-        String upperBound=args[4];
-        boolean methodFound=false;
-
+        String targetDirectory = args[0];
+        String mClass = args[1];
+        String tClass = args[2];
+        String tMethod = args[3];
+        String upperBound = args[4];
+        boolean methodFound = false;
 
         List<String> procDir = new ArrayList<String>();
         procDir.add(targetDirectory);
@@ -195,11 +224,12 @@ public class Analysis {
         SootMethod entryMethod = entryClass.getMethodByNameUnsafe("main");
         SootClass targetClass = Scene.v().getSootClassUnsafe(tClass);
         SootMethod targetMethod = entryClass.getMethodByNameUnsafe(tMethod);
-	    // A SootClass is Soot's representation of a class in the target program.
-	    // A SootMethod is Soot's representation fo a method in the target program.
-	    // Pay attention to the code above, as you may need to use the methods used
-	    // above at other places in your code as well to find a desired class or a desired method.
-	
+        // A SootClass is Soot's representation of a class in the target program.
+        // A SootMethod is Soot's representation fo a method in the target program.
+        // Pay attention to the code above, as you may need to use the methods used
+        // above at other places in your code as well to find a desired class or a
+        // desired method.
+
         Options.v().set_main_class(mClass);
         Scene.v().setEntryPoints(Collections.singletonList(entryMethod));
 
@@ -208,80 +238,84 @@ public class Analysis {
         System.out.println("tmethod: " + targetMethod);
         System.out.println("tmethodname: " + tMethod);
         Iterator mi = targetClass.getMethods().iterator();
-	    // targetClass.getMethods() retrieves all the methods in targetClass
+        // targetClass.getMethods() retrieves all the methods in targetClass
         while (mi.hasNext()) {
-            SootMethod sm = (SootMethod)mi.next();
-            if(sm.getName().equals(tMethod)) {
+            SootMethod sm = (SootMethod) mi.next();
+            if (sm.getName().equals(tMethod)) {
                 methodFound = true;
                 break;
             }
-	    // Not sure why this loop and check is required
+            // Not sure why this loop and check is required
         }
 
-        if(methodFound) {
+        if (methodFound) {
             printInfo(targetMethod);
-	    // the call above prints the Soot IR (Intermediate Represenation) of the targetMethod.
-	    // You can use the same printInfo method appropriately
-	    // to view the IR of any method.
+            // the call above prints the Soot IR (Intermediate Represenation) of the
+            // targetMethod.
+            // You can use the same printInfo method appropriately
+            // to view the IR of any method.
 
-	    // Note: Your analysis will work not on the source code of the target program, but on
-            // the Soot IR only. Soot automatically generates the IR of every method from the compiled
-	    // .class files. In other words, any Soot-based analysis needs only the .class files, and does
-	    // not need or use the source .java files. The IR is similar to a three-address code. 
+            // Note: Your analysis will work not on the source code of the target program,
+            // but on
+            // the Soot IR only. Soot automatically generates the IR of every method from
+            // the compiled
+            // .class files. In other words, any Soot-based analysis needs only the .class
+            // files, and does
+            // not need or use the source .java files. The IR is similar to a three-address
+            // code.
 
-	    /*****************************************************************
-             * XXX The function doAnalysis is the entry point for the Kildall's 
+            /*****************************************************************
+             * XXX The function doAnalysis is the entry point for the Kildall's
              * fix-point algorithm over the LatticeElement.
              ******************************************************************/
 
             drawMethodDependenceGraph(targetMethod);
-	    // the call above generates the control-flow graph of the targetMethod.
-	    // Since you are going to implement a flow-insensitive analysis, you
-	    // may not need to know about control-flow graphs at all. 
+            // the call above generates the control-flow graph of the targetMethod.
+            // Since you are going to implement a flow-insensitive analysis, you
+            // may not need to know about control-flow graphs at all.
 
-	        doAnalysis(targetMethod);
+            doAnalysis(targetMethod);
         } else {
             System.out.println("Method not found: " + tMethod);
         }
     }
 
-
-    private static void drawMethodDependenceGraph(SootMethod method){
-        if (!method.isPhantom() && method.isConcrete())
-        {
+    private static void drawMethodDependenceGraph(SootMethod method) {
+        if (!method.isPhantom() && method.isConcrete()) {
             Body body = method.retrieveActiveBody();
             ExceptionalUnitGraph graph = new ExceptionalUnitGraph(body);
 
             CFGToDotGraph cfgForMethod = new CFGToDotGraph();
             cfgForMethod.drawCFG(graph);
-            DotGraph cfgDot =  cfgForMethod.drawCFG(graph);
+            DotGraph cfgDot = cfgForMethod.drawCFG(graph);
             cfgDot.plot(method.getName() + "cfg.dot");
         }
     }
 
-    public static void printUnit(int lineno, Body b, Unit u){
+    public static void printUnit(int lineno, Body b, Unit u) {
         UnitPrinter up = new NormalUnitPrinter(b);
         u.toString(up);
         String linenostr = String.format("%02d", lineno) + ": ";
         System.out.println(linenostr + up.toString());
     }
 
-
     private static void printInfo(SootMethod entryMethod) {
-        if (!entryMethod.isPhantom() && entryMethod.isConcrete())
-        {
+        if (!entryMethod.isPhantom() && entryMethod.isConcrete()) {
             Body body = entryMethod.retrieveActiveBody();
-	        // `body' refers to the code body of entryMethod
+            // `body' refers to the code body of entryMethod
 
             int lineno = 0;
             for (Unit u : body.getUnits()) {
-        		// .getUnits retrieves all the Units in the code body of the method, as a list. 
-        		// A Unit basically represent a single statement or conditional in the Soot IR.
-        		// You should fully understand the structure of a Unit, the subtypes of Unit, etc., 
-        		// to make progress with your analysis.
-        		// Objects of type `Value' in Soot represent  local variables, constants and expressions.
-        		// Expressions may be BinopExp, InvokeExpr, and so on.
-        		// Boxes: References in Soot are called boxes. There are two types – Unitboxes, ValueBoxes.
+                // .getUnits retrieves all the Units in the code body of the method, as a list.
+                // A Unit basically represent a single statement or conditional in the Soot IR.
+                // You should fully understand the structure of a Unit, the subtypes of Unit,
+                // etc.,
+                // to make progress with your analysis.
+                // Objects of type `Value' in Soot represent local variables, constants and
+                // expressions.
+                // Expressions may be BinopExp, InvokeExpr, and so on.
+                // Boxes: References in Soot are called boxes. There are two types – Unitboxes,
+                // ValueBoxes.
 
                 if (!(u instanceof Stmt)) {
                     continue;
