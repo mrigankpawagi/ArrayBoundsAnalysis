ac565fcf9e7aa807d90084c5df1d30d19d17ca17
Mrigank Pawagi <mrigankpawagi@gmail.com>
2024-10-29 21:54:41 +0530

    fix IntervalElement to accomodate multiple variables; other cleanup + fixes

--- Analysis.java
+++ Analysis.java
@@ -32,43 +32,83 @@ public class Analysis {
 
         // Application of transfer functions (based on statements)
         LatticeElement tf_assignment(Stmt stmt);
+
+        // equals method
+        boolean equals(Object o);
     }
 
     public static class IntervalElement implements LatticeElement {
-        private final float lowerBound;
-        private final float upperBound;
-    
-        public IntervalElement(float lowerBound, float upperBound) {
-            this.lowerBound = lowerBound;
-            this.upperBound = upperBound;
+        private Map<Local, Pair<Float, Float>> intervalMap;
+        public static final IntervalElement bot = new IntervalElement();
+
+        public IntervalElement(Map<Local, Pair<Float, Float>> intervalMap) {
+            // check that all intervals are valid
+            for (Local local : intervalMap.keySet()) {
+                Pair<Float, Float> interval = intervalMap.get(local);
+                if (interval.first > interval.second) {
+                    throw new IllegalArgumentException("Invalid interval: " + interval);
+                }
+            }
+            this.intervalMap = intervalMap;
         }
-    
-        public float getLowerBound() {
-            return lowerBound;
+
+        // Private constructor for bot
+        private IntervalElement() {
+            this.intervalMap = null;
         }
-    
-        public float getUpperBound() {
-            return upperBound;
+
+        // check if the IntervalElement is bot
+        private boolean isBot() {
+            return this.intervalMap == null;
         }
 
         public LatticeElement join(LatticeElement other) {
             if (!(other instanceof IntervalElement)) {
                 throw new IllegalArgumentException("Incompatible types for join");
             }
-            IntervalElement otherInterval = (IntervalElement) other;
-            float newLower = Math.min(this.lowerBound, otherInterval.lowerBound);
-            float newUpper = Math.max(this.upperBound, otherInterval.upperBound);
-            return new IntervalElement(newLower, newUpper);
+            IntervalElement otherIntervalElement = (IntervalElement) other;
+            if (this.equals(bot)) {
+                return otherIntervalElement;
+            }
+            if (otherIntervalElement.equals(bot)) {
+                return this;
+            }
+            Map<Local, Pair<Float, Float>> newIntervalMap = new HashMap<>();
+            for (Local local : this.intervalMap.keySet()) {
+                Pair<Float, Float> thisInterval = this.intervalMap.get(local);
+                Pair<Float, Float> otherInterval = otherIntervalElement.intervalMap.get(local);
+                float newLower = Math.min(thisInterval.first, otherInterval.first);
+                float newUpper = Math.max(thisInterval.second, otherInterval.second);
+                newIntervalMap.put(local, new Pair<>(newLower, newUpper));
+            }
+            return new IntervalElement(newIntervalMap);
         }
 
         public LatticeElement tf_assignment(Stmt stmt) {
-            return new IntervalElement(Float.NEGATIVE_INFINITY, Float.POSITIVE_INFINITY);
+            if (this.equals(bot)) {
+                return bot; // bot is always transformed to bot
+            }
+            return bot; // TODO: Implement the transfer functions
+        }
+
+        public boolean equals(Object o) {
+            if (o instanceof IntervalElement) {
+                IntervalElement other = (IntervalElement) o;
+                if (this.isBot() && other.isBot()) {
+                    return true;
+                }
+                if (this.isBot() || other.isBot()) {
+                    return false;
+                }
+                return this.intervalMap.equals(other.intervalMap);
+            }
+            return false;
         }
 
         public String toString() {
-            return "[" + lowerBound + ", " + upperBound + "]";
+            return isBot() ? "bot" : intervalMap.toString();
         }
-    }    
+    }
 
     private DotGraph dot = new DotGraph("callgraph");
     private static HashMap<String, Boolean> visited = new HashMap<String, Boolean>();
@@ -195,6 +235,12 @@ public class Analysis {
         // ^ these three data structures are what we will need :)
     }
 
+    public static <T> Map<Unit, LatticeElement> runKildall(Class<? extends LatticeElement> latticeElementClass,
+            LatticeElement initialElement, Map<Integer, Set<Integer>> flowPoints,
+            Map<Pair<Integer, Integer>, Unit> enclosingUnit, Set<Pair<Integer, Integer>> trueBranches) {
+        return null; // TODO: Implement Kildall's algorithm
+    }
+
     public static void main(String[] args) {
 
         String targetDirectory = args[0];
@@ -224,11 +270,6 @@ public class Analysis {
         SootMethod entryMethod = entryClass.getMethodByNameUnsafe("main");
         SootClass targetClass = Scene.v().getSootClassUnsafe(tClass);
         SootMethod targetMethod = entryClass.getMethodByNameUnsafe(tMethod);
-        // A SootClass is Soot's representation of a class in the target program.
-        // A SootMethod is Soot's representation fo a method in the target program.
-        // Pay attention to the code above, as you may need to use the methods used
-        // above at other places in your code as well to find a desired class or a
-        // desired method.
 
         Options.v().set_main_class(mClass);
         Scene.v().setEntryPoints(Collections.singletonList(entryMethod));
@@ -249,31 +290,7 @@ public class Analysis {
         }
 
         if (methodFound) {
-            printInfo(targetMethod);
-            // the call above prints the Soot IR (Intermediate Represenation) of the
-            // targetMethod.
-            // You can use the same printInfo method appropriately
-            // to view the IR of any method.
-
-            // Note: Your analysis will work not on the source code of the target program,
-            // but on
-            // the Soot IR only. Soot automatically generates the IR of every method from
-            // the compiled
-            // .class files. In other words, any Soot-based analysis needs only the .class
-            // files, and does
-            // not need or use the source .java files. The IR is similar to a three-address
-            // code.
-
-            /*****************************************************************
-             * XXX The function doAnalysis is the entry point for the Kildall's
-             * fix-point algorithm over the LatticeElement.
-             ******************************************************************/
-
             drawMethodDependenceGraph(targetMethod);
-            // the call above generates the control-flow graph of the targetMethod.
-            // Since you are going to implement a flow-insensitive analysis, you
-            // may not need to know about control-flow graphs at all.
-
             doAnalysis(targetMethod);
         } else {
             System.out.println("Method not found: " + tMethod);
@@ -303,20 +320,8 @@ public class Analysis {
         if (!entryMethod.isPhantom() && entryMethod.isConcrete()) {
             Body body = entryMethod.retrieveActiveBody();
             // `body' refers to the code body of entryMethod
-
             int lineno = 0;
             for (Unit u : body.getUnits()) {
-                // .getUnits retrieves all the Units in the code body of the method, as a list.
-                // A Unit basically represent a single statement or conditional in the Soot IR.
-                // You should fully understand the structure of a Unit, the subtypes of Unit,
-                // etc.,
-                // to make progress with your analysis.
-                // Objects of type `Value' in Soot represent local variables, constants and
-                // expressions.
-                // Expressions may be BinopExp, InvokeExpr, and so on.
-                // Boxes: References in Soot are called boxes. There are two types – Unitboxes,
-                // ValueBoxes.
-
                 if (!(u instanceof Stmt)) {
                     continue;
                 }
