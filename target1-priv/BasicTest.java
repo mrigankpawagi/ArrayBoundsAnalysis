dd82a53b10d65fc3b9f87907ec49559c92018fec
Alan Jojo <alanjojo@proton.me>
2024-11-13 19:49:58 +0530

    Starting Phase 2

--- target1-priv/BasicTest.java
+++ target1-priv/BasicTest.java
@@ -1,30 +1,18 @@
-class BasicTest{
-    static int fun2(){
-        int x = 2;
-        for(int i = 0; i < 5; i++){
-            x = x*2;
+class BasicTest {
+    static void foo(int n) {
+        int i = n;
+        while (i > 10) {
+            i--;
         }
-        return x;
-    }
-    static int fun1(int check){
-        int x = 10;
-        if(check < 100){
-            x = x - 1;
+        int j = 9;
+        int k = 1;
+        while (j < 18) { 
+            j = j + k; 
+            k = k + 1; 
         }
-        else{
-            x = x + 1;
+        if (i == j) {
+            int l = i;
         }
-
-        return x+check;
-
-    }
-
-    static int fun3(){
-        int x = 0;
-        while(x >=0){
-            x = x + 2;
-        }
-        return x;
     }
 }
 
